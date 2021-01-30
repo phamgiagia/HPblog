d553242e9991e5f519113b3d03f89b956054a9d1
phamgiagia <phamgiagia@gmail.com>
2021-01-31 03:43:20 +0700

    Update intro.js

--- components/intro.js
+++ components/intro.js
@@ -4,17 +4,17 @@ export default function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Hai Pham Blog.
+        Hai Pham.
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        Update with {CMS_NAME}, things that 
+        Update with {CMS_NAME} Blog,{' '}
         <a
           href="#!"
           className="underline hover:text-success duration-200 transition-colors"
         >
-          I
+          Technology, life story and more.
         </a>{' '}
-        feel excited about.
+        .
       </h4>
     </section>
   )
